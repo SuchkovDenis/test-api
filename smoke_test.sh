4b6b1e58a040e1bae52110e6bc41d996eba067eb
Денис Сучков <ny1828@yandex.ru>
2019-11-29 06:17:19 +0300

    Added smoke test

---
+++ smoke_test.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+status=`curl --silent --head https://api.hh.ru/vacancies | head -1 | cut -f 2 -d' '`
+
+if [ "$status" != "200" ]
+then
+	echo "SMOKE TEST FAILED"
+	exit 1
+else
+	echo "SMOKE TEST PASSED"
+	exit 0
+fi
